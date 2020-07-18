68b58ba6972ecb752adeeacc2ae28779514fae52
MORIMORI <morimori.0317@outlook.jp>
2020-07-19 00:17:58 +0900

    当たり判定邸

--- src/main/java/red/felnull/otyacraftengine/item/TestItem.java
+++ src/main/java/red/felnull/otyacraftengine/item/TestItem.java
@@ -1,17 +1,23 @@
 package red.felnull.otyacraftengine.item;
 
+import net.minecraft.block.AnvilBlock;
+import net.minecraft.block.Block;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import red.felnull.otyacraftengine.OtyacraftEngine;
-import red.felnull.otyacraftengine.data.WorldDataManager;
 
-//@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class TestItem extends Item implements IDetailedInfomationItem {
 
     public TestItem(Properties properties) {
@@ -19,23 +25,20 @@ public class TestItem extends Item implements IDetailedInfomationItem {
 
     }
 
+
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand hand) {
         ItemStack item = playerIn.getHeldItem(hand);
 
-        //   if (worldIn.isRemote) {
+        if (!worldIn.isRemote) {
+        //    playerIn.sendStatusMessage(new StringTextComponent(X_AXIS_AABB.toString()), false);
 
-        //  if (playerIn.isCrouching()) {
-        //   WorldDataManager.instance().getPlayerData(playerIn, new ResourceLocation(OtyacraftEngine.MODID, "test")).putInt("test", 510);
-        //   } else {
-        playerIn.sendStatusMessage(new StringTextComponent(WorldDataManager.instance().getPlayerData(playerIn, new ResourceLocation(OtyacraftEngine.MODID, "test"), worldIn.isRemote).toString()), false);
-        //   }
-        //   }
+        }
 
         return ActionResult.func_233538_a_(item, worldIn.isRemote());
     }
 
-/*
+
     @SubscribeEvent
     public static void onItemsRegistry(final RegistryEvent.Register<Item> e) {
 
@@ -43,5 +46,5 @@ public class TestItem extends Item implements IDetailedInfomationItem {
                 .setRegistryName(OtyacraftEngine.MODID, "test"));
 
     }
-*/
+
 }
--- src/main/java/red/felnull/otyacraftengine/util/VoxelShapeHelper.java
+++ src/main/java/red/felnull/otyacraftengine/util/VoxelShapeHelper.java
@@ -1,7 +1,12 @@
 package red.felnull.otyacraftengine.util;
 
 import net.minecraft.block.Block;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class VoxelShapeHelper {
     public static VoxelShape makeCuboidShaoe0(double x1, double y1, double z1, double x2, double y2, double z2) {
@@ -34,5 +39,48 @@ public class VoxelShapeHelper {
 
     public static VoxelShape addCuboidShaoe270(double x1, double y1, double z1, double x2, double y2, double z2) {
         return makeCuboidShaoe270(x1, y1, z1, x1 + x2, y1 + y2, z1 + z2);
+    }
+
+    public static VoxelShape allOr(VoxelShape... shapes) {
+        if (shapes.length == 1)
+            return shapes[0];
+        List<VoxelShape> shapesll = new ArrayList<VoxelShape>();
+        VoxelShape fristV = null;
+        boolean isFrist = false;
+        for (VoxelShape box : shapes) {
+            if (!isFrist) {
+                isFrist = true;
+                fristV = box;
+            } else {
+                shapesll.add(box);
+            }
+        }
+        return VoxelShapes.or(fristV, shapesll.toArray(new VoxelShape[1]));
+    }
+
+    public static VoxelShape translate(VoxelShape shape, double x, double y, double z) {
+        List<VoxelShape> shapes = new ArrayList<VoxelShape>();
+        shape.toBoundingBoxList().forEach(n -> shapes.add(VoxelShapes.create(n.offset(1f / 16f * x, 1f / 16f * y, 1f / 16f * z))));
+        return allOr(shapes.toArray(new VoxelShape[1]));
+    }
+
+    public static VoxelShape rotate90(VoxelShape shape) {
+        List<VoxelShape> shapes = new ArrayList<VoxelShape>();
+        shape.toBoundingBoxList().forEach(n -> shapes.add(VoxelShapes.create(new AxisAlignedBB(n.minZ, n.minY, 1 - n.minX, n.maxZ, n.maxY, 1 - n.maxX))));
+        return allOr(shapes.toArray(new VoxelShape[1]));
+    }
+
+    public static VoxelShape rotate180(VoxelShape shape) {
+        List<VoxelShape> shapes = new ArrayList<VoxelShape>();
+        shape.toBoundingBoxList().forEach(n -> shapes.add(VoxelShapes.create(new AxisAlignedBB(1 - n.minX, n.minY, 1 - n.minZ, 1 - n.maxX, n.maxY, 1 - n.maxZ))));
+        return allOr(shapes.toArray(new VoxelShape[1]));
+    }
+
+
+    public static VoxelShape rotate270(VoxelShape shape) {
+        shape = rotate180(shape);
+        List<VoxelShape> shapes = new ArrayList<VoxelShape>();
+        shape.toBoundingBoxList().forEach(n -> shapes.add(VoxelShapes.create(new AxisAlignedBB(n.minZ, n.minY, 1 - n.minX, n.maxZ, n.maxY, 1 - n.maxX))));
+        return allOr(shapes.toArray(new VoxelShape[1]));
     }
 }
