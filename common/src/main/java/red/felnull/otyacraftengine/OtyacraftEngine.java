4c964f7b8e2339c5cabecc5ba8932501e64bfd4a
MORIMORI0317 <morimori.0317@outlook.jp>
2021-02-14 16:07:40 +0900

    use architectury api

---
+++ common/src/main/java/red/felnull/otyacraftengine/OtyacraftEngine.java
@@ -0,0 +1,9 @@
+package red.felnull.otyacraftengine;
+
+public class OtyacraftEngine {
+    public static final String MODID = "otyacraftengine";
+
+    public static void init() {
+
+    }
+}
---
+++ fabric/src/main/java/red/felnull/otyacraftengine/OtyacraftEngineFabric.java
@@ -0,0 +1,10 @@
+package red.felnull.otyacraftengine;
+
+import net.fabricmc.api.ModInitializer;
+
+public class OtyacraftEngineFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        OtyacraftEngine.init();
+    }
+}
