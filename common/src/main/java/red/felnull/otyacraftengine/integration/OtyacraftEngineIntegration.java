126e01124451184dbb9eafed820b02021f657a4e
MORIMORI0317 <morimori.0317@outlook.jp>
2021-02-16 12:07:13 +0900

    class handler

--- common/src/main/java/red/felnull/otyacraftengine/integration/OtyacraftEngineIntegration.java
+++ common/src/main/java/red/felnull/otyacraftengine/integration/OtyacraftEngineIntegration.java
@@ -2,14 +2,14 @@ package red.felnull.otyacraftengine.integration;
 
 import red.felnull.otyacraftengine.api.IOEIntegration;
 import red.felnull.otyacraftengine.api.OEIntegration;
-import red.felnull.otyacraftengine.api.event.TestEvent;
 import red.felnull.otyacraftengine.api.register.OEHandlerRegister;
 import red.felnull.otyacraftengine.handler.TestHandler;
 
 @OEIntegration
 public class OtyacraftEngineIntegration implements IOEIntegration {
     @Override
-    public void registrationHandler(OEHandlerRegister hdrg) {
-        hdrg.register(TestEvent.class, TestHandler::onTest);
+    public void registrationHandler(OEHandlerRegister reg) {
+        reg.register(TestHandler.class);
+        // reg.register(TestEvent.class, TestHandler::onTest);
     }
 }
