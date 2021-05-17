dabaf6483403daab3589c6f5db73b717bd89646f
MORIMORI0317 <morimori.0317@outlook.jp>
2021-05-17 15:06:31 +0900

    url util

---
+++ common/src/main/java/red/felnull/otyacraftengine/util/IKSGURLUtil.java
@@ -0,0 +1,24 @@
+package red.felnull.otyacraftengine.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class IKSGURLUtil {
+    public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36";
+
+    public static String getURLResponse(String url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.addRequestProperty("user-agent", USER_AGENT);
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
+        StringBuilder sb = new StringBuilder();
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            sb.append(inputLine).append('\n');
+        in.close();
+        return sb.toString();
+    }
+}
