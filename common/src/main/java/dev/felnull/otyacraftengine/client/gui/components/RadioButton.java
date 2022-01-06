81271910dd58dd4c8aaa74b942de1fd4348d320a
MORIMORI0317 <morimori.0317@outlook.jp>
2022-01-07 02:51:00 +0900

    fix rdo button

--- common/src/main/java/dev/felnull/otyacraftengine/client/gui/components/RadioButton.java
+++ common/src/main/java/dev/felnull/otyacraftengine/client/gui/components/RadioButton.java
@@ -10,6 +10,7 @@ import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class RadioButton extends AbstractButton implements IOEBaseComponent {
@@ -22,13 +23,14 @@ public class RadioButton extends AbstractButton implements IOEBaseComponent {
     private final int textureHeight;
     private final int textureSizeWidth;
     private final int textureSizeHeight;
+    private final Consumer<RadioButton> onPress;
     private boolean selected;
 
-    public RadioButton(int x, int y, int w, int h, Component title, boolean selected, boolean showLabel, Supplier<RadioButton[]> group) {
-        this(x, y, w, h, title, selected, showLabel, group, WIDGETS, 0, 0, 20, 20, 256, 256);
+    public RadioButton(int x, int y, int w, int h, Component title, boolean selected, boolean showLabel, Supplier<RadioButton[]> group, Consumer<RadioButton> onPress) {
+        this(x, y, w, h, title, selected, showLabel, group, WIDGETS, 0, 0, 20, 20, 256, 256, onPress);
     }
 
-    public RadioButton(int x, int y, int w, int h, Component title, boolean selected, boolean showLabel, Supplier<RadioButton[]> group, ResourceLocation texture, int tx, int ty, int tw, int th, int tsX, int tsY) {
+    public RadioButton(int x, int y, int w, int h, Component title, boolean selected, boolean showLabel, Supplier<RadioButton[]> group, ResourceLocation texture, int tx, int ty, int tw, int th, int tsX, int tsY, Consumer<RadioButton> onPress) {
         super(x, y, w, h, title);
         this.selected = selected;
         this.showLabel = showLabel;
@@ -40,6 +42,7 @@ public class RadioButton extends AbstractButton implements IOEBaseComponent {
         this.textureHeight = th;
         this.textureSizeWidth = tsX;
         this.textureSizeHeight = tsY;
+        this.onPress = onPress;
     }
 
     @Override
@@ -51,6 +54,7 @@ public class RadioButton extends AbstractButton implements IOEBaseComponent {
                 rdo.selected = false;
             }
         }
+        this.onPress.accept(this);
     }
 
     public boolean selected() {
@@ -71,10 +75,14 @@ public class RadioButton extends AbstractButton implements IOEBaseComponent {
 
     @Override
     public void renderButton(PoseStack poseStack, int i, int j, float f) {
-        OERenderUtil.drawTexture(texture, poseStack, x, y, textureX + (this.isFocused() ? 20 : 0), textureY + (this.selected ? 20 : 0), textureWidth, textureHeight, textureSizeWidth, textureSizeHeight);
+        OERenderUtil.drawTexture(texture, poseStack, x, y, textureX + (this.isHovered() ? 20 : 0), textureY + (this.selected ? 20 : 0), textureWidth, textureHeight, textureSizeWidth, textureSizeHeight);
         this.renderBg(poseStack, mc, i, j);
         if (this.showLabel) {
-            drawString(poseStack, mc.font, this.getMessage(), this.x + 24, this.y + (this.height - 8) / 2, 14737632 | Mth.ceil(this.alpha * 255.0F) << 24);
+            drawRdoString(poseStack, this.getMessage(), this.x + 24, this.y + (this.height - 8) / 2, 14737632 | Mth.ceil(this.alpha * 255.0F) << 24);
         }
+    }
+
+    public void drawRdoString(PoseStack poseStack, Component component, int i, int j, int k) {
+        drawString(poseStack, mc.font, component, i, j, k);
     }
 }
