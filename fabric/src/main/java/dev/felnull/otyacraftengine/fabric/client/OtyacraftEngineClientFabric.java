242406949a1631a81290836d964c7e90b78fec5f
MORIMORI0317 <morimori.0317@outlook.jp>
2022-02-28 17:31:02 +0900

    nna

--- fabric/src/main/java/dev/felnull/otyacraftengine/fabric/client/OtyacraftEngineClientFabric.java
+++ fabric/src/main/java/dev/felnull/otyacraftengine/fabric/client/OtyacraftEngineClientFabric.java
@@ -3,13 +3,6 @@ package dev.felnull.otyacraftengine.fabric.client;
 import dev.felnull.otyacraftengine.client.OtyacraftEngineClient;
 import dev.felnull.otyacraftengine.fabric.client.handler.ModelResourceHandler;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
-import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.renderer.item.ClampedItemPropertyFunction;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.ItemStack;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class OtyacraftEngineClientFabric implements ClientModInitializer {
     @Override
