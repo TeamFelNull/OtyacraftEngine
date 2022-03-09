12d7afc3e3124af11d022331a856781905a001d0
MORIMORI0317 <morimori.0317@outlook.jp>
2022-03-10 04:45:47 +0900

    nna

---
+++ forge/src/main/java/dev/felnull/otyacraftengine/impl/forge/OEPatchouliExpectPlatformImpl.java
@@ -0,0 +1,17 @@
+package dev.felnull.otyacraftengine.impl.forge;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import org.jetbrains.annotations.Nullable;
+import vazkii.patchouli.api.PatchouliAPI;
+
+public class OEPatchouliExpectPlatformImpl {
+    public static void openBookGUI(ServerPlayer player, ResourceLocation location) {
+        PatchouliAPI.get().openBookGUI(player, location);
+    }
+
+    @Nullable
+    public static ResourceLocation getOpenBookGui() {
+        return PatchouliAPI.get().getOpenBookGui();
+    }
+}
