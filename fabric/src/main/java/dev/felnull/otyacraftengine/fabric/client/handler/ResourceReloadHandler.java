eb052b16dcdd4eda646c059c2404987563b86be4
MORIMORI0317 <morimori.0317@outlook.jp>
2022-04-11 17:01:27 +0900

    fix myron conflict

--- fabric/src/main/java/dev/felnull/otyacraftengine/fabric/client/handler/ResourceReloadHandler.java
+++ fabric/src/main/java/dev/felnull/otyacraftengine/fabric/client/handler/ResourceReloadHandler.java
@@ -5,25 +5,8 @@ import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.server.packs.PackType;
 
 public class ResourceReloadHandler {
-    //    public static final ResourceLocation OE_3D_MODEL = new ResourceLocation(OtyacraftEngine.MODID, "3d_model");
 
     public static void init() {
         ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(new MotionManagerFabric());
-       /* ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(new SimpleResourceReloadListener<OEModelLoader>() {
-            @Override
-            public ResourceLocation getFabricId() {
-                return OE_3D_MODEL;
-            }
-
-            @Override
-            public CompletableFuture<OEModelLoader> load(ResourceManager manager, ProfilerFiller profiler, Executor executor) {
-                return CompletableFuture.supplyAsync(() -> OEModelLoader.load(manager), executor);
-            }
-
-            @Override
-            public CompletableFuture<Void> apply(OEModelLoader data, ResourceManager manager, ProfilerFiller profiler, Executor executor) {
-                return CompletableFuture.runAsync(data::apply, executor);
-            }
-        });*/
     }
 }
