6f47d37a862133bb529b70f2a90c93fbe7f3d3bd
MORIMORI0317 <morimori.0317@outlook.jp>
2022-06-10 00:58:21 +0900

    add dependencies

--- common/src/main/java/dev/felnull/otyacraftengine/OtyacraftEngine.java
+++ common/src/main/java/dev/felnull/otyacraftengine/OtyacraftEngine.java
@@ -1,7 +1,11 @@
 package dev.felnull.otyacraftengine;
 
+import dev.felnull.fnjl.util.FNURLUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class OtyacraftEngine {
     public static final Logger LOGGER = LogManager.getLogger(OtyacraftEngine.class);
@@ -9,5 +13,12 @@ public class OtyacraftEngine {
 
     public static void init() {
         LOGGER.info("ｳｧｧ!!ｵﾚﾓｲｯﾁｬｳｩｩｩ!!!ｳｳｳｳｳｳｳｳｳｩｩｩｩｩｩｩｩｳｳｳｳｳｳｳｳ!ｲｨｨｲｨｨｨｲｲｲｨｲｲｲｲ");
+
+        try {
+            var r = FNURLUtil.getResponse(new URL("https://www.morimori0317.net/inc-sounds-search/search?s=%E3%81%84%E3%81%8D%E3%81%99%E3%81%8E"));
+            System.out.println(r);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
