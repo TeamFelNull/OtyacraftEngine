4294bec79a386f60a534f2405c89b32cdb10b05f
MORIMORI0317 <morimori.0317@outlook.jp>
2022-06-10 10:39:24 +0900

    add test project

--- common/src/main/java/dev/felnull/otyacraftengine/OtyacraftEngine.java
+++ common/src/main/java/dev/felnull/otyacraftengine/OtyacraftEngine.java
@@ -8,6 +8,10 @@ public class OtyacraftEngine {
     public static final String MODID = "otyacraftengine";
 
     public static void init() {
-       
+
+    }
+
+    public static String getTest() {
+        return "課長壊れる";
     }
 }
