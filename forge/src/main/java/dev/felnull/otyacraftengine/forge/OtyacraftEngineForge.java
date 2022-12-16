b1d5b9180b0ca6bea8b691fb3cf856c596c4686e
MORIMORI0317 <morimori.0317@outlook.jp>
2022-12-16 18:59:24 +0900

    nna

--- forge/src/main/java/dev/felnull/otyacraftengine/forge/OtyacraftEngineForge.java
+++ forge/src/main/java/dev/felnull/otyacraftengine/forge/OtyacraftEngineForge.java
@@ -3,7 +3,6 @@ package dev.felnull.otyacraftengine.forge;
 import dev.architectury.platform.forge.EventBuses;
 import dev.felnull.otyacraftengine.OtyacraftEngine;
 import dev.felnull.otyacraftengine.forge.handler.CommonHandlerForge;
-import dev.felnull.otyacraftengine.forge.server.handler.ServerHandlerForge;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -12,7 +11,6 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class OtyacraftEngineForge {
     public OtyacraftEngineForge() {
         MinecraftForge.EVENT_BUS.register(CommonHandlerForge.class);
-        MinecraftForge.EVENT_BUS.register(ServerHandlerForge.class);
         EventBuses.registerModEventBus(OtyacraftEngine.MODID, FMLJavaModLoadingContext.get().getModEventBus());
         OtyacraftEngine.init();
     }
